1ee64b1636c63fd300dc40dcb5aaab27efe432ee
relentless-coder01 <relentless.coder01@gmail.com>
2024-06-23 12:31:41 -0400

    Server run file with Gunicorn

---
+++ server_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --chdir app
